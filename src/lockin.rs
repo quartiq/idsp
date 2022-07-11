8c70f533e1864e20371777d912a316e64903667b
Robert Jördens <rj@quartiq.de>
2022-07-11 21:19:06 +0200

    lockin: add update_iq()

--- src/lockin.rs
+++ src/lockin.rs
@@ -6,10 +6,9 @@ pub struct Lockin<const N: usize> {
 }
 
 impl<const N: usize> Lockin<N> {
-    /// Update the lockin with a sample taken at a given phase.
-    pub fn update(&mut self, sample: i32, phase: i32, k: u32) -> Complex<i32> {
-        // Get the LO signal for demodulation and mix the sample;
-        let mix = Complex::from_angle(phase).mul_scaled(sample);
+    /// Update the lockin with a sample taken at a local oscillator IQ value.
+    pub fn update_iq(&mut self, sample: i32, lo: Complex<i32>, k: u32) -> Complex<i32> {
+        let mix = lo.mul_scaled(sample);
 
         // Filter with the IIR lowpass,
         // return IQ (in-phase and quadrature) data.
@@ -17,5 +16,11 @@ impl<const N: usize> Lockin<N> {
             re: self.state[0].update(mix.re, k),
             im: self.state[1].update(mix.im, k),
         }
+    }
+
+    /// Update the lockin with a sample taken at a given phase.
+    pub fn update(&mut self, sample: i32, phase: i32, k: u32) -> Complex<i32> {
+        // Get the LO signal for demodulation and mix the sample;
+        self.update_iq(sample, Complex::from_angle(phase), k)
     }
 }
