e23c1bfa8f7545100379961dae2380da6d72208c
Robert Jördens <rj@quartiq.de>
2022-08-13 21:20:24 +0200

    complex: remove non-functional doctests
    These worked only because of a significant residual bias in the old
    algorithm. The new algorithm is bias-free but not error free for those
    values.

--- src/complex.rs
+++ src/complex.rs
@@ -74,12 +74,7 @@ impl ComplexExt<i32, u32> for Complex<i32> {
     ///
     /// ```
     /// use idsp::{Complex, ComplexExt};
-    /// assert_eq!(Complex::new(1, 0).arg(), 0);
-    /// assert_eq!(Complex::new(-i32::MAX, 1).arg(), i32::MAX);
-    /// assert_eq!(Complex::new(-i32::MAX, -1).arg(), i32::MIN);
-    /// assert_eq!(Complex::new(0, -1).arg(), (i32::MIN >> 1) - 1);
-    /// assert_eq!(Complex::new(0, 1).arg(), (i32::MAX >> 1) + 1);
-    /// assert_eq!(Complex::new(1, 1).arg(), (i32::MAX >> 2) + 1);
+    /// assert_eq!(Complex::new(0, 0).arg(), 0);
     /// ```
     fn arg(&self) -> i32 {
         atan2(self.im, self.re)
