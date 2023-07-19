0d74a53c69387c34f36336ca32a84ab76986157b
Robert Jördens <rj@quartiq.de>
2023-07-19 18:19:21 +0000

    support lowpass2 in locking

--- src/lockin.rs
+++ src/lockin.rs
@@ -1,25 +1,35 @@
 use super::{Chain, Complex, ComplexExt, Filter, Lowpass, MulScaled};
 
 #[derive(Copy, Clone, Default)]
-pub struct Lockin<const N: usize> {
-    state: [Chain<N, Lowpass<1>>; 2],
+pub struct Lockin<const N: usize, const O: usize> {
+    state: [Chain<N, Lowpass<O>>; 2],
 }
 
-impl<const N: usize> Lockin<N> {
+impl<const N: usize, const O: usize> Lockin<N, O> {
     /// Update the lockin with a sample taken at a local oscillator IQ value.
-    pub fn update_iq(&mut self, sample: i32, lo: Complex<i32>, k: i32) -> Complex<i32> {
+    pub fn update_iq(
+        &mut self,
+        sample: i32,
+        lo: Complex<i32>,
+        k: &<Lowpass<O> as Filter>::Config,
+    ) -> Complex<i32> {
         let mix = lo.mul_scaled(sample);
 
         // Filter with the IIR lowpass,
         // return IQ (in-phase and quadrature) data.
         Complex {
-            re: self.state[0].update(mix.re, &[k]),
-            im: self.state[1].update(mix.im, &[k]),
+            re: self.state[0].update(mix.re, k),
+            im: self.state[1].update(mix.im, k),
         }
     }
 
     /// Update the lockin with a sample taken at a given phase.
-    pub fn update(&mut self, sample: i32, phase: i32, k: i32) -> Complex<i32> {
+    pub fn update(
+        &mut self,
+        sample: i32,
+        phase: i32,
+        k: &<Lowpass<O> as Filter>::Config,
+    ) -> Complex<i32> {
         // Get the LO signal for demodulation and mix the sample;
         self.update_iq(sample, Complex::from_angle(phase), k)
     }
