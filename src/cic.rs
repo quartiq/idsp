0529d5307a1f55caf3f36d46dbb8efa25ac7f465
Robert Jördens <rj@quartiq.de>
2024-06-18 10:21:05 +0200

    cic tweaks

--- src/cic.rs
+++ src/cic.rs
@@ -1,31 +1,6 @@
-use core::iter::{repeat, Repeat, Take};
+use core::ops::AddAssign;
 
 use num_traits::{Num, WrappingAdd};
-
-/// Comb stage
-#[derive(Clone, Copy, PartialEq, PartialOrd, Debug, Default)]
-pub struct Comb<T>(T);
-
-impl<T: Num + Copy> Comb<T> {
-    /// Ingest a new sample into the filter and return its current output.
-    pub fn update(&mut self, x: T) -> T {
-        let y = x - self.0;
-        self.0 = x;
-        y
-    }
-}
-
-/// Integrators stage
-#[derive(Clone, Copy, PartialEq, PartialOrd, Debug, Default)]
-pub struct Integrator<T>(T);
-
-impl<T: Num + WrappingAdd + Copy> Integrator<T> {
-    /// Ingest a new sample into the filter and return its current output.
-    pub fn update(&mut self, x: T) -> T {
-        self.0 = self.0.wrapping_add(&x);
-        self.0
-    }
-}
 
 /// Cascaded integrator comb interpolatpr
 ///
@@ -33,24 +8,22 @@ impl<T: Num + WrappingAdd + Copy> Integrator<T> {
 #[derive(Clone, Debug)]
 pub struct CicInterpolator<T, const N: usize> {
     rate: usize,
-    combs: [Comb<T>; N],
-    up: Take<Repeat<T>>,
-    integrators: [Integrator<T>; N],
+    combs: [T; N],
+    up: T,
+    index: usize,
+    integrators: [T; N],
 }
 
-impl<T: Num + WrappingAdd + Copy + Default, const N: usize> CicInterpolator<T, N> {
-    /// Return the filter gain
-    pub const fn gain(&self) -> usize {
-        self.rate.pow(N as _)
-    }
-
+impl<T: Num + AddAssign + Copy, const N: usize> CicInterpolator<T, N> {
     /// Create a new zero-initialized filter with the given rate change.
     pub fn new(rate: usize) -> Self {
+        debug_assert!(rate > 0);
         Self {
             rate,
-            combs: [Comb::default(); N],
-            up: repeat(T::zero()).take(rate),
-            integrators: [Integrator::default(); N],
+            combs: [T::zero(); N],
+            up: T::zero(),
+            index: 0,
+            integrators: [T::zero(); N],
         }
     }
 
@@ -58,12 +31,26 @@ impl<T: Num + WrappingAdd + Copy + Default, const N: usize> CicInterpolator<T, N
     /// retrieve the next output.
     pub fn update(&mut self, x: Option<T>) -> T {
         if let Some(x) = x {
-            let x = self.combs.iter_mut().fold(x, |x, c| c.update(x));
-            self.up = repeat(x).take(self.rate);
+            let x = self.combs.iter_mut().fold(x, |x, c| {
+                let y = x - *c;
+                *c = x;
+                y
+            });
+            debug_assert_eq!(self.index, 0);
+            self.index = self.rate - 1;
+            self.up = x;
+        } else {
+            self.index -= 1;
         }
-        self.integrators
-            .iter_mut()
-            .fold(self.up.next().unwrap(), |x, i| i.update(x))
+        self.integrators.iter_mut().fold(self.up, |x, i| {
+            *i += x;
+            *i
+        })
+    }
+
+    /// Return the filter gain
+    pub const fn gain(&self) -> usize {
+        self.rate.pow(N as _)
     }
 }
 
@@ -73,38 +60,46 @@ impl<T: Num + WrappingAdd + Copy + Default, const N: usize> CicInterpolator<T, N
 #[derive(Clone, Debug)]
 pub struct CicDecimator<T, const N: usize> {
     rate: usize,
-    integrators: [Integrator<T>; N],
+    integrators: [T; N],
     index: usize,
-    combs: [Comb<T>; N],
+    combs: [T; N],
 }
 
-impl<T: Num + WrappingAdd + Copy + Default, const N: usize> CicDecimator<T, N> {
-    /// Return the filter gain
-    pub fn gain(&self) -> usize {
-        self.rate.pow(N as _)
-    }
-
+impl<T: Num + WrappingAdd + Copy, const N: usize> CicDecimator<T, N> {
     /// Create a new zero-initialized filter with the given rate change.
     pub fn new(rate: usize) -> Self {
         debug_assert!(rate > 0);
         Self {
             rate,
-            combs: [Comb::default(); N],
+            combs: [T::zero(); N],
             index: 0,
-            integrators: [Integrator::default(); N],
+            integrators: [T::zero(); N],
         }
     }
 
     /// Ingest a new high-rate sample and optionally retrieve next output.
     pub fn update(&mut self, x: T) -> Option<T> {
-        let x = self.integrators.iter_mut().fold(x, |x, i| i.update(x));
+        let x = self.integrators.iter_mut().fold(x, |x, i| {
+            // Overflow is OK if bitwidth is sufficient (input * gain)
+            *i = i.wrapping_add(&x);
+            *i
+        });
         if self.index == 0 {
             self.index = self.rate - 1;
-            let x = self.combs.iter_mut().fold(x, |x, c| c.update(x));
+            let x = self.combs.iter_mut().fold(x, |x, c| {
+                let y = x - *c;
+                *c = x;
+                y
+            });
             Some(x)
         } else {
             self.index -= 1;
             None
         }
+    }
+
+    /// Return the filter gain
+    pub fn gain(&self) -> usize {
+        self.rate.pow(N as _)
     }
 }
