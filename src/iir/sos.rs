17d6790bf1ebbf30d6a0d343e14c99186cd1a507
Robert Jördens <rj@quartiq.de>
2025-11-18 17:52:26 +0100

    sos: Process

--- src/iir/sos.rs
+++ src/iir/sos.rs
@@ -1,10 +1,55 @@
+use num_traits::Float as _;
+
 /// Processing block
 /// Single input, single output, one value
-pub trait Block {
-    /// The operating state
-    type State;
+pub trait Process<State> {
     /// Update the state with a new sample and obtain an output sample
-    fn process(&self, state: &mut Self::State, x0: i32) -> i32;
+    fn process(&self, state: &mut State, x: i32) -> i32;
+
+    /// Process a block of samples
+    fn process_block(&self, state: &mut State, x: &[i32], y: &mut [i32]) {
+        for (x, y) in x.iter().zip(y) {
+            *y = self.process(state, *x);
+        }
+    }
+
+    /// Process a block of samples inplace
+    fn process_in_place(&self, state: &mut State, xy: &mut [i32]) {
+        for xy in xy {
+            *xy = self.process(state, *xy);
+        }
+    }
+}
+
+/// A cascade of several filter blocks of the same type
+#[derive(Clone, Debug, Default)]
+#[repr(transparent)]
+pub struct Cascade<T>(pub T);
+impl<T, const N: usize> Process<[State; N]> for Cascade<[T; N]>
+where
+    T: Process<State>,
+{
+    fn process(&self, state: &mut [State; N], x: i32) -> i32 {
+        self.0
+            .iter()
+            .zip(state)
+            .fold(x, |x, (filter, state)| filter.process(state, x))
+    }
+
+    fn process_block(&self, state: &mut [State; N], x: &[i32], y: &mut [i32]) {
+        if let Some((filter, state)) = self.0.first().zip(state.first_mut()) {
+            filter.process_block(state, x, y);
+        }
+        for (filter, state) in self.0.iter().zip(state).skip(1) {
+            filter.process_in_place(state, y);
+        }
+    }
+
+    fn process_in_place(&self, state: &mut [State; N], xy: &mut [i32]) {
+        for (filter, state) in self.0.iter().zip(state) {
+            filter.process_in_place(state, xy);
+        }
+    }
 }
 
 /// Second-order-section
@@ -21,35 +66,9 @@ pub struct Sos<const Q: u8> {
     pub ba: [i32; 5],
 }
 
-/// SOS state
-#[derive(Clone, Debug, Default)]
-pub struct State {
-    /// X,Y state
-    ///
-    /// `[x1, x2, y1, y2]`
-    pub xy: [i32; 4],
-}
-
-impl<const Q: u8> Block for Sos<Q> {
-    type State = State;
-
-    fn process(&self, state: &mut Self::State, x0: i32) -> i32 {
-        let mut acc = 0;
-        acc += x0 as i64 * self.ba[0] as i64;
-        acc += state.xy[0] as i64 * self.ba[1] as i64;
-        acc += state.xy[1] as i64 * self.ba[2] as i64;
-        // no subtraction/negation for performance, apply that to coefficients
-        acc += state.xy[2] as i64 * self.ba[3] as i64;
-        acc += state.xy[3] as i64 * self.ba[4] as i64;
-        let y0 = (acc >> Q) as i32;
-        state.xy = [x0, state.xy[0], y0, state.xy[2]];
-        y0
-    }
-}
-
-/// Second-order-section
-#[derive(Clone, Debug, Default)]
-pub struct SosR<const Q: u8> {
+/// Second-order-section with offset and clamp
+#[derive(Clone, Debug)]
+pub struct SosClamp<const Q: u8> {
     /// Coefficients
     ///
     /// `[b0, b1, b2, a1, a2]`
@@ -61,30 +80,132 @@ pub struct SosR<const Q: u8> {
     pub ba: [i32; 5],
     /// Summing junction offset
     pub u: i32,
-    /// Output min clamp
+    /// Summing junction min clamp
     pub min: i32,
-    /// Output min clamp
+    /// Summing junction min clamp
     pub max: i32,
+}
+
+impl<const Q: u8> Default for SosClamp<Q> {
+    fn default() -> Self {
+        Self {
+            ba: Default::default(),
+            u: 0,
+            min: i32::MIN,
+            max: i32::MAX,
+        }
+    }
+}
+
+impl<const Q: u8> SosClamp<Q> {
+    /// DC gain
+    pub fn k(&self) -> i32 {
+        self.ba[..3].iter().sum::<i32>()
+    }
+
+    /// Summing junction offset referred to input
+    pub fn input_offset(&self) -> i32 {
+        (((self.u as i64) << Q) / self.k() as i64) as _
+    }
+
+    /// Summing junction offset referred to input
+    pub fn set_input_offset(&mut self, i: i32) {
+        self.u = ((i as i64 * self.k() as i64) >> Q) as _;
+    }
 }
 
 /// SOS state
 #[derive(Clone, Debug, Default)]
-pub struct StateR {
+pub struct State {
+    /// X,Y state
+    ///
+    /// `[x1, x2, y1, y2]`
+    pub xy: [i32; 4],
+}
+
+/// SOS state with wide Y
+#[derive(Clone, Debug, Default)]
+pub struct StateWide {
     /// X state
     ///
     /// `[x1, x2]`
     pub x: [i32; 2],
     /// Y state
     ///
-    /// `[y1, y2]``
+    /// `[y1, y2]`
     pub y: [i64; 2],
 }
 
-impl<const Q: u8> Block for SosR<Q> {
-    type State = StateR;
+/// SOS state with first order error feedback
+#[derive(Clone, Debug, Default)]
+pub struct StateDither {
+    /// X,Y state
+    ///
+    /// `[x1, x2, y1, y2]`
+    pub xy: [i32; 4],
+    /// Error feedback
+    pub e: u32,
+}
 
-    fn process(&self, state: &mut Self::State, x0: i32) -> i32 {
-        let StateR { x, y } = state;
+impl<const Q: u8> Process<State> for Sos<Q> {
+    fn process(&self, state: &mut State, x0: i32) -> i32 {
+        let xy = &mut state.xy;
+        let ba = &self.ba;
+        let mut acc = 0;
+        acc += x0 as i64 * ba[0] as i64;
+        acc += xy[0] as i64 * ba[1] as i64;
+        acc += xy[1] as i64 * ba[2] as i64;
+        acc += xy[2] as i64 * ba[3] as i64;
+        acc += xy[3] as i64 * ba[4] as i64;
+        let y0 = (acc >> Q) as i32;
+        *xy = [x0, xy[0], y0, xy[2]];
+        y0
+    }
+}
+
+impl<const Q: u8> Process<State> for SosClamp<Q> {
+    fn process(&self, state: &mut State, x0: i32) -> i32 {
+        let xy = &mut state.xy;
+        let ba = &self.ba;
+        let mut acc = 0;
+        acc += x0 as i64 * ba[0] as i64;
+        acc += xy[0] as i64 * ba[1] as i64;
+        acc += xy[1] as i64 * ba[2] as i64;
+        acc += xy[2] as i64 * ba[3] as i64;
+        acc += xy[3] as i64 * ba[4] as i64;
+        let mut y0 = (acc >> Q) as i32 + self.u;
+        if y0 < self.min {
+            y0 = self.min;
+        } else if y0 > self.max {
+            y0 = self.max;
+        }
+        *xy = [x0, xy[0], y0, xy[2]];
+        y0
+    }
+}
+
+impl<const Q: u8> Process<StateWide> for Sos<Q> {
+    fn process(&self, state: &mut StateWide, x0: i32) -> i32 {
+        let StateWide { x, y } = state;
+        let ba = &self.ba;
+        let mut acc = 0;
+        acc += x0 as i64 * ba[0] as i64;
+        acc += x[0] as i64 * ba[1] as i64;
+        acc += x[1] as i64 * ba[2] as i64;
+        *x = [x0, x[0]];
+        acc += (y[0] as u32 as i64 * ba[3] as i64) >> 32;
+        acc += (y[0] >> 32) * ba[3] as i64;
+        acc += (y[1] as u32 as i64 * ba[4] as i64) >> 32;
+        acc += (y[1] >> 32) * ba[4] as i64;
+        acc <<= 32 - Q;
+        *y = [acc, y[0]];
+        (acc >> 32) as _
+    }
+}
+
+impl<const Q: u8> Process<StateWide> for SosClamp<Q> {
+    fn process(&self, state: &mut StateWide, x0: i32) -> i32 {
+        let StateWide { x, y } = state;
         let ba = &self.ba;
         let mut acc = 0;
         acc += x0 as i64 * ba[0] as i64;
@@ -103,6 +224,47 @@ impl<const Q: u8> Block for SosR<Q> {
             y0 = self.max;
         }
         *y = [((y0 as i64) << 32) | acc as u32 as i64, y[0]];
+        y0
+    }
+}
+
+impl<const Q: u8> Process<StateDither> for Sos<Q> {
+    fn process(&self, state: &mut StateDither, x0: i32) -> i32 {
+        let StateDither { xy, e } = state;
+        let ba = &self.ba;
+        let mut acc = *e as i64;
+        acc += x0 as i64 * ba[0] as i64;
+        acc += xy[0] as i64 * ba[1] as i64;
+        acc += xy[1] as i64 * ba[2] as i64;
+        acc += xy[2] as i64 * ba[3] as i64;
+        acc += xy[3] as i64 * ba[4] as i64;
+        acc <<= 32 - Q;
+        *e = acc as _;
+        let y0 = (acc >> 32) as i32;
+        *xy = [x0, xy[0], y0, xy[2]];
+        y0
+    }
+}
+
+impl<const Q: u8> Process<StateDither> for SosClamp<Q> {
+    fn process(&self, state: &mut StateDither, x0: i32) -> i32 {
+        let StateDither { xy, e } = state;
+        let ba = &self.ba;
+        let mut acc = *e as i64;
+        acc += x0 as i64 * ba[0] as i64;
+        acc += xy[0] as i64 * ba[1] as i64;
+        acc += xy[1] as i64 * ba[2] as i64;
+        acc += xy[2] as i64 * ba[3] as i64;
+        acc += xy[3] as i64 * ba[4] as i64;
+        acc <<= 32 - Q;
+        *e = acc as _;
+        let mut y0 = (acc >> 32) as i32 + self.u;
+        if y0 < self.min {
+            y0 = self.min;
+        } else if y0 > self.max {
+            y0 = self.max;
+        }
+        *xy = [x0, xy[0], y0, xy[2]];
         y0
     }
 }
@@ -134,3 +296,48 @@ impl<const Q: u8> Block for SosR<Q> {
 //         }
 //     }
 // }
+
+fn quantize(ba: &[[f64; 3]; 2], q: u8) -> [[i32; 3]; 2] {
+    let a0 = (1u64 << q) as f64 / ba[1][0];
+    ba.map(|b| b.map(|b| (b * a0).round() as i32))
+}
+
+fn flip(ba: [[i32; 3]; 2]) -> [i32; 5] {
+    [ba[0][0], ba[0][1], ba[0][2], -ba[1][1], -ba[1][2]]
+}
+
+impl<const Q: u8> From<&[[f64; 3]; 2]> for Sos<Q> {
+    fn from(ba: &[[f64; 3]; 2]) -> Self {
+        Self {
+            ba: flip(quantize(ba, Q)),
+        }
+    }
+}
+
+impl<const Q: u8> From<&[[f64; 3]; 2]> for SosClamp<Q> {
+    fn from(ba: &[[f64; 3]; 2]) -> Self {
+        Self {
+            ba: flip(quantize(ba, Q)),
+            ..Default::default()
+        }
+    }
+}
+
+impl<const Q: u8> From<[i32; 5]> for Sos<Q> {
+    fn from(mut ba: [i32; 5]) -> Self {
+        ba[3] *= -1;
+        ba[4] *= -1;
+        Self { ba }
+    }
+}
+
+impl<const Q: u8> From<[i32; 5]> for SosClamp<Q> {
+    fn from(mut ba: [i32; 5]) -> Self {
+        ba[3] *= -1;
+        ba[4] *= -1;
+        Self {
+            ba,
+            ..Default::default()
+        }
+    }
+}
