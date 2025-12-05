78b8874d146b68710763529aed01e045b028aff5
Robert Jördens <rj@quartiq.de>
2025-12-05 13:17:46 +0100

    lowpass: port to Process

--- src/lowpass.rs
+++ src/lowpass.rs
@@ -1,4 +1,4 @@
-use crate::Filter;
+use crate::iir::{Process, StatefulRef};
 
 /// Arbitrary order, high dynamic range, wide coefficient range,
 /// lowpass filter implementation. DC gain is 1.
@@ -9,9 +9,14 @@ use crate::Filter;
 ///
 /// Both filters have been optimized for accuracy, dynamic range, and
 /// speed on Cortex-M7.
-#[derive(Copy, Clone)]
-pub struct Lowpass<const N: usize>(pub(crate) [i64; N]);
-impl<const N: usize> Filter for Lowpass<N> {
+#[derive(Clone, Debug)]
+pub struct Lowpass<const N: usize>(pub [i32; N]);
+
+/// Lowpass filter state
+#[derive(Clone, Debug)]
+pub struct LowpasState<const N: usize>(pub [i64; N]);
+
+impl<const N: usize> Process<i32> for StatefulRef<'_, Lowpass<N>, LowpasState<N>> {
     /// The filter configuration `Config` contains the filter gains.
     ///
     /// For the first-order lowpass this is a single element array `[k]` with
@@ -30,37 +35,29 @@ impl<const N: usize> Filter for Lowpass<N> {
     /// `1 <= k <= (1 << 31) - 1`.
     /// The second-order lowpass works and is accurate for
     /// `1 << 16 <= k <= q*(1 << 31)`.
-    type Config = [i32; N];
-    fn update(&mut self, x: i32, k: &Self::Config) -> i32 {
-        let mut d = x.saturating_sub(self.get()) as i64 * k[0] as i64;
+    fn process(&mut self, x: i32) -> i32 {
+        let mut d =
+            x.saturating_sub((self.state.0[0] >> 32) as i32) as i64 * self.config.0[0] as i64;
         let y;
         if N == 1 {
-            self.0[0] += d;
-            y = self.get();
-            self.0[0] += d;
+            self.state.0[0] += d;
+            y = (self.state.0[0] >> 32) as i32;
+            self.state.0[0] += d;
         } else if N == 2 {
-            d += (self.0[1] >> 32) * k[1] as i64;
-            self.0[1] += d;
-            self.0[0] += self.0[1];
-            y = self.get();
+            d += (self.state.0[1] >> 32) * self.config.0[1] as i64;
+            self.state.0[1] += d;
+            self.state.0[0] += self.state.0[1];
+            y = (self.state.0[0] >> 32) as i32;
             // This creates the double Nyquist zero,
             // compensates the gain lost in the signed i32 as (i32 as i64)*(i64 >> 32)
             // multiplication while keeping the lowest bit significant, and
             // copes better with wrap-around than Nyquist averaging.
-            self.0[0] += self.0[1];
-            self.0[1] += d;
+            self.state.0[0] += self.state.0[1];
+            self.state.0[1] += d;
         } else {
             unimplemented!()
         }
         y
-    }
-
-    fn get(&self) -> i32 {
-        (self.0[0] >> 32) as i32
-    }
-
-    fn set(&mut self, x: i32) {
-        self.0[0] = (x as i64) << 32;
     }
 }
 
