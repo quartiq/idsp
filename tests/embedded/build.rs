226b0e0ae09ce3f83e611e2ec904bc7b75e1e44f
Robert Jördens <rj@quartiq.de>
2026-01-25 14:34:54 +0000

    bench: reduce

--- tests/embedded/build.rs
+++ tests/embedded/build.rs
@@ -4,16 +4,12 @@ use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
-    // std::fs::write(out.join("memory.x"), include_bytes!("memory.x")).unwrap();
     println!("cargo:rustc-link-search={}", out.display());
     println!("cargo:rustc-link-arg-bins=--nmagic");
-    //println!("cargo:rustc-link-arg-bins=-Tmemory.x");
-    //println!("cargo:rerun-if-changed=memory.x");
 
     println!("cargo:rustc-link-arg-bins=-Tlink_ram.x");
     println!("cargo:rustc-link-arg-bins=-Tdevice.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
-    //println!("cargo:rustc-link-arg-bins=-Tteleprobe.x");
 
     Ok(())
 }
--- tests/embedded/src/bin/bench.rs
+++ tests/embedded/src/bin/bench.rs
@@ -3,103 +3,16 @@
 
 use core::{
     hint::black_box,
-    ptr,
     sync::atomic::{self, Ordering},
 };
 use cortex_m::{asm, peripheral::DWT};
 use defmt::*;
-// use embassy_stm32::{bind_interrupts, peripherals};
-use embassy_stm32::Config;
-use embassy_stm32::rcc::*;
 use num_traits::Float;
-use {defmt_rtt as _, panic_probe as _};
+use {defmt_rtt as _, embassy_stm32 as _, panic_probe as _};
 
 use dsp_fixedpoint::Q32;
 use dsp_process::{Inplace, Process, Split};
 use idsp::iir;
-
-fn init() -> embassy_stm32::Peripherals {
-    let mut config = Config::default();
-    config.rcc.hsi = Some(HSIPrescaler::DIV1);
-    config.rcc.csi = true;
-    config.rcc.hsi48 = Some(Default::default());
-    config.rcc.pll1 = Some(Pll {
-        source: PllSource::HSI,
-        prediv: PllPreDiv::DIV4,
-        mul: PllMul::MUL50,
-        divp: Some(PllDiv::DIV2),
-        divq: Some(PllDiv::DIV8),
-        divr: None,
-        fracn: None,
-    });
-    config.rcc.pll2 = Some(Pll {
-        source: PllSource::HSI,
-        prediv: PllPreDiv::DIV4,
-        mul: PllMul::MUL50,
-        divp: Some(PllDiv::DIV8), // 100 Mhz
-        divq: None,
-        divr: None,
-        fracn: None,
-    });
-    config.rcc.sys = Sysclk::PLL1_P; // 400 Mhz
-    config.rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
-    config.rcc.apb1_pre = APBPrescaler::DIV2; // 100 Mhz
-    config.rcc.apb2_pre = APBPrescaler::DIV2; // 100 Mhz
-    config.rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
-    config.rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
-    config.rcc.voltage_scale = VoltageScale::Scale1;
-    config.rcc.mux.adcsel = mux::Adcsel::PLL2_P;
-    // config.rcc.supply_config = SupplyConfig::DirectSMPS;
-    embassy_stm32::init(config)
-}
-
-/// Setup ITCM and load its code from flash.
-///
-/// For portability and maintainability this is implemented in Rust.
-/// Since this is implemented in Rust the compiler may assume that bss and data are set
-/// up already. There is no easy way to ensure this implementation will never need bss
-/// or data. Hence we can't safely run this as the cortex-m-rt `pre_init` hook before
-/// bss/data is setup.
-///
-/// Calling (through IRQ or directly) any code in ITCM before having called
-/// this method is undefined.
-fn load_itcm() {
-    unsafe extern "C" {
-        // ZST (`()`: not layout-stable. empty/zst struct in `repr(C)``: not "proper" C)
-        unsafe static mut __sitcm: [u32; 0];
-        unsafe static mut __eitcm: [u32; 0];
-        unsafe static mut __siitcm: [u32; 0];
-    }
-    // NOTE(unsafe): Assuming the address symbols from the linker as well as
-    // the source instruction data are all valid, this is safe as it only
-    // copies linker-prepared data to where the code expects it to be.
-    // Calling it multiple times is safe as well.
-
-    // ITCM is enabled on reset on our CPU but might not be on others.
-    // Keep for completeness.
-    const ITCMCR: *mut u32 = 0xE000_EF90usize as _;
-    unsafe {
-        ptr::write_volatile(ITCMCR, ptr::read_volatile(ITCMCR) | 1);
-    }
-
-    // Ensure ITCM is enabled before loading.
-    atomic::fence(Ordering::SeqCst);
-
-    let sitcm = ptr::addr_of_mut!(__sitcm) as *mut u32;
-    let eitcm = ptr::addr_of!(__eitcm) as *const u32;
-    let siitcm = ptr::addr_of!(__siitcm) as *const u32;
-
-    unsafe {
-        let len = eitcm.offset_from(sitcm) as usize;
-        // Load code into ITCM.
-        ptr::copy(siitcm, sitcm, len);
-    }
-
-    // Ensure ITCM is loaded before potentially executing any instructions from it.
-    atomic::fence(Ordering::SeqCst);
-    cortex_m::asm::dsb();
-    cortex_m::asm::isb();
-}
 
 fn time<F: FnMut()>(mut func: F) -> u32 {
     cortex_m::interrupt::free(|_cs| {
@@ -115,13 +28,10 @@ fn time<F: FnMut()>(mut func: F) -> u32 {
 }
 
 #[inline(never)]
-// #[unsafe(link_section = ".itcm.timeit")]
 fn noinline<F: FnOnce()>(func: F) {
     func()
 }
 
-//#[inline(never)]
-//#[unsafe(link_section = ".itcm.timeit")]
 fn timeit<F: FnMut()>(mut func: F) -> u32 {
     let c1 = time(|| {
         noinline(&mut func);
@@ -143,7 +53,7 @@ struct Cycles {
 struct CyclesResults {
     single: u32,
     chunk: Cycles,
-    large: Cycles,
+    slice: Cycles,
 }
 
 impl CyclesResults {
@@ -158,8 +68,8 @@ impl CyclesResults {
                 self.single as f32,
                 self.chunk.block as f32 / CHUNK as f32,
                 self.chunk.inplace as f32 / CHUNK as f32,
-                self.large.block as f32 / SLICE as f32,
-                self.large.inplace as f32 / SLICE as f32,
+                self.slice.block as f32 / SLICE as f32,
+                self.slice.inplace as f32 / SLICE as f32,
             ]
         );
     }
@@ -180,14 +90,14 @@ where
     });
     let mut xy = [black_box([x; SLICE]); 2];
     let [x, y] = xy.each_mut().map(|x| x.as_mut_slice());
-    let large = Cycles {
+    let slice = Cycles {
         block: timeit(|| proc.block(x, y)),
         inplace: timeit(|| proc.inplace(y)),
     };
     let ((x, []), (y, [])) = (x.as_chunks::<CHUNK>(), y.as_chunks_mut::<CHUNK>()) else {
         defmt::unreachable!()
     };
-    // let large = Cycles {
+    // let chunked = Cycles {
     //     block: timeit(|| {
     //         for (x, y) in x.iter().zip(y.iter_mut()) {
     //             proc.block(x, y);
@@ -207,17 +117,17 @@ where
     CyclesResults {
         single,
         chunk,
-        large,
+        slice,
     }
 }
 
-struct BiquadAdapter<B>(B);
-impl<B: biquad::Biquad<X>, X: Copy + Float> dsp_process::Process<X> for BiquadAdapter<B> {
+struct BiquadProcess<B>(B);
+impl<B: biquad::Biquad<X>, X: Copy + Float> dsp_process::Process<X> for BiquadProcess<B> {
     fn process(&mut self, x: X) -> X {
         self.0.run(x)
     }
 }
-impl<B, X: Copy> dsp_process::Inplace<X> for BiquadAdapter<B> where Self: dsp_process::Process<X> {}
+impl<B, X: Copy> dsp_process::Inplace<X> for BiquadProcess<B> where Self: dsp_process::Process<X> {}
 
 fn coefficients_default<T: Float>() -> biquad::Coefficients<T> {
     biquad::Coefficients {
@@ -232,11 +142,9 @@ fn coefficients_default<T: Float>() -> biquad::Coefficients<T> {
 #[cortex_m_rt::entry]
 fn main() -> ! {
     info!("Setup");
-    let _p = init();
     let mut c = unwrap!(cortex_m::Peripherals::take());
 
-    load_itcm();
-
+    // Roughly halves cycles
     // c.SCB.enable_icache();
     // c.SCB.enable_dcache(&mut c.CPUID);
 
@@ -281,7 +189,7 @@ fn main() -> ! {
     bench_process(&mut Split::<iir::Biquad<f32>, iir::DirectForm1<f32>>::default())
         .show("idsp f32");
 
-    bench_process(&mut Split::stateful(BiquadAdapter(
+    bench_process(&mut Split::stateful(BiquadProcess(
         biquad::DirectForm1::new(coefficients_default::<f32>()),
     )))
     .show("biquad df1 f32");
@@ -292,7 +200,7 @@ fn main() -> ! {
     >::default())
     .show("idsp df2t f32");
 
-    bench_process(&mut Split::stateful(BiquadAdapter(
+    bench_process(&mut Split::stateful(BiquadProcess(
         biquad::DirectForm2Transposed::new(coefficients_default::<f32>()),
     )))
     .show("biquad df2t f32");
@@ -303,7 +211,7 @@ fn main() -> ! {
     bench_process(&mut Split::<iir::Biquad<f64>, iir::DirectForm1<f64>>::default())
         .show("idsp f64");
 
-    bench_process(&mut Split::stateful(BiquadAdapter(
+    bench_process(&mut Split::stateful(BiquadProcess(
         biquad::DirectForm1::new(coefficients_default::<f64>()),
     )))
     .show("biquad df1 f64");
@@ -314,7 +222,7 @@ fn main() -> ! {
     >::default())
     .show("idsp df2t f64");
 
-    bench_process(&mut Split::stateful(BiquadAdapter(
+    bench_process(&mut Split::stateful(BiquadProcess(
         biquad::DirectForm2Transposed::new(coefficients_default::<f64>()),
     )))
     .show("biquad df2t f64");
@@ -327,7 +235,7 @@ fn main() -> ! {
     bench_process::<_, _, i32>(&mut Split::new(
         Pair::new((
             (
-                Unsplit(&Identity),
+                (),
                 Parallel((
                     [
                         defmt::unwrap!(Wdf::<_, 0xad>::quantize(&[-0.9, 0.9])),
@@ -339,9 +247,9 @@ fn main() -> ! {
                     ),
                 )),
             ),
-            Unsplit(&Add),
+            (),
         )),
-        Default::default(),
+        ((Unsplit(Identity), Default::default()), Unsplit(Add)),
     ))
     .show("idsp wdf-ca-7");
 
